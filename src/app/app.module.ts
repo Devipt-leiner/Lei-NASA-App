21e1ef10eef14001b4e082069f69eff6d65341e0
Leiner Barrios Medina <leiner.barrios@inglesparatodos.edu.co>
2022-07-24 02:22:49 -0500

    Mars rovers photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PrimeNgModule } from './prime-ng/prime-ng/prime-ng.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NasaService } from './core/services/nasa.service';
 
 import { AppComponent } from './app.component';
 import { LeiButtonComponent } from './components/atoms/lei-button/lei-button.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { EarthTemplateComponent } from './components/templates/earth-template/earth-template.component';
+import { MarsTemplateComponent } from './components/templates/mars-template/mars-template.component';
+import { MarsCardComponent } from './components/organisms/mars-card/mars-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeiButtonComponent,
-    HomePageComponent
+    HomePageComponent,
+    EarthTemplateComponent,
+    MarsTemplateComponent,
+    MarsCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    NasaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { NasaService } from 'src/app/core/services/nasa.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent implements OnInit {
+  activeItem!: MenuItem;
+  items!: MenuItem[];
+  template: string = 'earth';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.items = [
+      {
+        label: 'Earth',
+        icon: 'pi pi-fw pi-home',
+        command: (event) => {
+          this.template = 'earth';
+        },
+      },
+      {
+        label: 'Mars Rover Fotos',
+        icon: 'pi pi-fw pi-images',
+        command: (event) => {
+          this.template = 'mars';
+        },
+      },
+    ];
+    this.activeItem = this.items[0];
   }
-
 }
