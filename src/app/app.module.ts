1a1cb2f1fd13048a1888fa813e406e64c986e80a
Leiner Barrios Medina <leiner.barrios@inglesparatodos.edu.co>
2022-07-24 00:17:43 -0500

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeiButtonComponent } from './components/atoms/lei-button/lei-button.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeiButtonComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
